2f5cde522d46c1665c9be32bf79f4d82ecd8c947
ivana <ivana.djipalo.00@fesb.hr>
2021-05-14 18:27:22 +0200

    styles

--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -1,9 +1,10 @@
 import React from "react";
 import CharacterItem from "./CharacterItem";
+import "../../App.css";
 
 const CharacterGrid = ({ items, isLoading }) => {
   return isLoading ? (
-    <span>Loading</span>
+    <h2 className="loading">Loading</h2>
   ) : (
     <section className="container cards">
       {items.data.results.map((item) => (
